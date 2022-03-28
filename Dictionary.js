8b5e06f40a7d37a1169c652b201a5bb94012b152
mariellaaaa <101421492+mariellaaaa@users.noreply.github.com>
2022-03-28 11:46:38 +0300

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,19 +1,21 @@
-let WordsList = [];
+let wordsList = [];
+
 function addWords() {
-  let template = WordsList.map(word => `<li>${word}</li>`).join('\n');
+  let template = wordsList.map(word => `<li>${word}</li>`).join('\n');
 }
 
 let buttonAdd = document.querySelector('button');
 let input = document.querySelector('input');
 
 buttonAdd.addEventListener('click', () =>{
-  WordsList.push(input.value);
+  wordsList.push(input.value);
   input.value = '';
   addWords();
 });
+
 function verifyWord() {
   let value = document.getElementById('wordToverify').value;
-  if (WordsList.indexOf(value) > -1) {
+  if (wordsList.indexOf(value) > -1) {
     document.getElementById('outputtext').innerHTML = value + " " + "exists in the Dictionary";
   } else {
     document.getElementById('outputtext').innerHTML = value + " " + "doesn't exist in the Dictionary";
